c4c68e611512debdb3c0ffd679dd39f3b0c8849b
mfrancis95 <mikefrancis95@gmail.com>
2014-12-21 18:19:43 -0500

    Added Entity class.

---
+++ src/com/amf/engine/Entity.java
@@ -0,0 +1,15 @@
+package com.amf.engine;
+
+public class Entity {
+    
+    private Location screenLocation;
+    
+    public Location getScreenLocation() {
+        return screenLocation;
+    }
+    
+    public void setScreenLocation(Location location) {
+        screenLocation = location;
+    }
+    
+}
