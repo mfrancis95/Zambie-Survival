04c023d6586666a24a8c588e721c833abacc2f8a
mfrancis95 <mikefrancis95@gmail.com>
2014-12-21 00:31:51 -0500

    Tileset is now implemented as a HashMap of tiles.

--- src/com/amf/engine/Tileset.java
+++ src/com/amf/engine/Tileset.java
@@ -2,15 +2,23 @@ package com.amf.engine;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.imageio.ImageIO;
 
 public class Tileset {
     
-    private BufferedImage image;
+    private Map<Location, BufferedImage> tiles;
     
     public Tileset(String resource) {
         try {
-            image = ImageIO.read(getClass().getResource("/resources/" + resource));
+            BufferedImage image = ImageIO.read(getClass().getResource("/resources/" + resource));
+            tiles = new HashMap<>();
+            for (int x = 0; x < image.getWidth(); x += 32) {
+                for (int y = 0; y < image.getHeight(); y += 32) {
+                    tiles.put(new Location(x / 32, y / 32), image.getSubimage(x, y, 32, 32));
+                }
+            }
         } 
         catch (IOException ex) {
             System.out.println(ex);
@@ -18,7 +26,7 @@ public class Tileset {
     }
     
     public BufferedImage getTile(int x, int y) {
-        return image.getSubimage(x, y, 32, 32);
+        return tiles.get(new Location(x, y));
     }
     
 }
