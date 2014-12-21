8c596dafdd6326bf90c2953b6dd7c4f339c37529
mfrancis95 <mikefrancis95@gmail.com>
2014-12-21 00:33:27 -0500

    GridMap now contains a HashMap of tiles in addition to entities. The tiles are represented as their coordinates in the tileset instead of the actual images.

--- src/com/amf/engine/GridMap.java
+++ src/com/amf/engine/GridMap.java
@@ -9,13 +9,20 @@ public class GridMap<T> {
     
     private final Map<Location, T> entities;
     
+    private final Map<Location, Location> tiles;
+    
     public GridMap(int squareSize) {
         this.squareSize = squareSize;
         entities = new HashMap<>();
+        tiles = new HashMap<>();
     }
     
     public void addEntity(T entity, int x, int y) {
         entities.put(new Location(x, y), entity);
+    }
+    
+    public void addTile(Location location, int x, int y) {
+        tiles.put(new Location(x, y), location);
     }
     
     public T getEntity(int x, int y) {
@@ -29,6 +36,10 @@ public class GridMap<T> {
             }
         }
         return null;
+    }
+    
+    public Location getTile(int x, int y) {
+        return tiles.get(new Location(x, y));
     }
     
     public void move(Location from, Location to) {
