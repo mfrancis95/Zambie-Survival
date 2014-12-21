35bda0579abcc7e37741642c9dd5820756575c6c
mfrancis95 <mikefrancis95@gmail.com>
2014-12-21 01:00:13 -0500

    Finalised fallback and tiles fields.

--- src/com/amf/engine/Tileset.java
+++ src/com/amf/engine/Tileset.java
@@ -8,20 +8,20 @@ import javax.imageio.ImageIO;
 
 public class Tileset {
     
-    private Map<Location, BufferedImage> tiles;
+    private final BufferedImage fallback;
     
-    private BufferedImage fallback;
+    private final Map<Location, BufferedImage> tiles;
     
     public Tileset(String resource, int tileSize) {
+        fallback = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_RGB);
+        tiles = new HashMap<>();
         try {
             BufferedImage image = ImageIO.read(getClass().getResource("/resources/" + resource));
-            tiles = new HashMap<>();
             for (int x = 0; x < image.getWidth(); x += tileSize) {
                 for (int y = 0; y < image.getHeight(); y += tileSize) {
                     tiles.put(new Location(x / tileSize, y / tileSize), image.getSubimage(x, y, tileSize, tileSize));
                 }
             }
-            fallback = new BufferedImage(tileSize, tileSize, BufferedImage.TYPE_INT_RGB);
         } 
         catch (IOException ex) {
             System.out.println(ex);
