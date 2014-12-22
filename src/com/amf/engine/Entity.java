c08a399b7282456c9fbb12a000553b26cad319d1
RTann <RTann95@gmail.com>
2014-12-22 14:45:17 -0500

    Did some stuff

--- src/com/amf/engine/Entity.java
+++ src/com/amf/engine/Entity.java
@@ -2,14 +2,32 @@ package com.amf.engine;
 
 import java.io.Serializable;
 
-public class Entity implements Serializable {
-    
+public abstract class Entity implements Serializable {
+
     private Location screenLocation;
+
+    private int health;
     
+    public Entity(Location startLocation, int maxHealth) {
+        screenLocation = startLocation;
+        health = maxHealth;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
     public Location getScreenLocation() {
         return screenLocation;
     }
     
+    public abstract void performAction(String action);
+
+    // or just this.health = health depending on damage calculations
+    public void setHealth(int change) {
+        health = Math.max(0, change);
+    }
+
     public void setScreenLocation(Location location) {
         screenLocation = location;
     }
---
+++ src/com/amf/engine/Survivor.java
@@ -0,0 +1,45 @@
+package com.amf.engine;
+
+public class Survivor extends Entity {
+
+    //can it ever be full? Money/credit system???
+    private Inventory inventory;
+
+    public Survivor(Location startLocation, int maxHealth) {
+        super(startLocation, maxHealth);
+    }
+
+    @Override
+    public void performAction(String action) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private class Inventory {
+
+        /* int representing the number of items of a certain kind
+         *defaulted to 10 unique items. Each item can go in specific spot.
+         *if no like, can switch back to HashMap. This way seems easier to handle 
+         given we have an invariant of which item goes where */
+        int[] inventory = new int[10];
+
+        public void addToInventory(int index) {
+            inventory[index] += 1;
+        }
+        
+        public boolean containsItem(int index) {
+            return inventory[index] != 0;
+        }
+        
+        private void getInventory() {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+        
+        //Assumes contains item was called already because idk if we 
+        //want to do error handling
+        public void removeItem(int index) {
+            inventory[index] -= 1;
+        }
+
+    }
+
+}
