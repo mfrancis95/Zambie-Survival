1ded2595bcf612fd3bc7a98a8b0bd45b947209ac
mfrancis95 <mikefrancis95@gmail.com>
2014-12-22 22:27:23 -0500

    Updated to support the new map size (20x15). Also prevented the user from being table to select a tile outside of the tileset.

--- src/com/amf/builder/Builder.java
+++ src/com/amf/builder/Builder.java
@@ -100,7 +100,10 @@ public class Builder extends JFrame {
     private final MouseListener tilesetMouse = new MouseAdapter() {
         
         public void mousePressed(MouseEvent me) {
-            selectedTile = new Location(me.getX() / 32, me.getY() / 32);
+            Location location = new Location(me.getX() / 32, me.getY() / 32);
+            if (tileset.getTile(location) != tileset.getFallbackTile()) {
+                selectedTile = location;
+            }
         }
         
     };
@@ -149,23 +152,23 @@ public class Builder extends JFrame {
             super();
             addMouseListener(mapMouse);
             addMouseMotionListener(mapMouse);
-            setPreferredSize(new Dimension(720, 480));
+            setPreferredSize(new Dimension(640, 480));
         }
         
         public void paintComponent(Graphics bork) {
             Graphics2D g = (Graphics2D) bork;
-            for (int x = 0; x < 22; x++) {
+            for (int x = 0; x < 20; x++) {
                 for (int y = 0; y < 15; y++) {
                     Location location = map.getTile(x, y);
                     g.drawImage(tileset.getTile(location), null, x * 32, y * 32);
                 }
             }
             g.setColor(Color.GRAY);
-            for (int x = 0; x < 720; x += 32) {
-                g.drawLine(x, 0, x, 480);
+            for (int x = 0; x < 640; x += 32) {
+                g.drawLine(x, 0, x, 640);
             }
-            for (int y = 0; y < 480; y += 32) {
-                g.drawLine(0, y, 720, y);
+            for (int y = 0; y < 640; y += 32) {
+                g.drawLine(0, y, 640, y);
             }
             g.setColor(Color.RED);
             g.setStroke(new BasicStroke(2));
@@ -183,8 +186,8 @@ public class Builder extends JFrame {
         
         public void paintComponent(Graphics bork) {
             Graphics2D g = (Graphics2D) bork;
-            for (int x = 0; x < 22; x++) {
-                for (int y = 0; y < 15; y++) {
+            for (int x = 0; x < 6; x++) {
+                for (int y = 0; y < 10; y++) {
                     g.drawImage(tileset.getTile(new Location(x, y)), null, x * 32, y * 32);
                 }
             }
