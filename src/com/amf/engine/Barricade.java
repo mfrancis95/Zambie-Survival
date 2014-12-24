18fad9abc0c4d734e7007bbbb632b6e86641b77a
RTann <RTann95@gmail.com>
2014-12-23 21:24:05 -0500

    implements Item as well as extends Entity

--- src/com/amf/engine/Barricade.java
+++ src/com/amf/engine/Barricade.java
@@ -1,6 +1,8 @@
 package com.amf.engine;
 
-public class Barricade extends Entity {
+public class Barricade extends Entity implements Item {
+    
+    public static final int MAX_QUANTITY = 5;
 
     @Override
     public Direction getDirection() {
@@ -15,6 +17,11 @@ public class Barricade extends Entity {
     @Override
     public void setDirection(Direction direction) {
         throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public void use(Survivor owner, Entity target, GridMap map) {
+        //map.addTile(null, MAX_QUANTITY, MAX_QUANTITY);
     }
     
 }
