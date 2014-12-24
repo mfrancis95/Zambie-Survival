8df6dfd5d3603287f862e23025c78f8f10b0a805
RTann <RTann95@gmail.com>
2014-12-24 18:35:27 -0500

    Added if statement: "isAbleToUse" to check if the item is even a valid option (such as within the range of the item). Maybe should be placed where the item is called rather than in the item, though...

--- src/com/amf/engine/Bandage.java
+++ src/com/amf/engine/Bandage.java
@@ -3,10 +3,15 @@ package com.amf.engine;
 public class Bandage implements Item {
 
     public static final int MAX_QUANTITY = 5, RECOVER = 10;
-    
+
     @Override
     public void use(Survivor owner, Entity target, GridMap map) {
-        owner.setHealth(Math.min(Survivor.MAX_HEALTH, owner.getHealth() + RECOVER));
+        //if (ableToUseItem) {
+            owner.setHealth(Math.min(Survivor.MAX_HEALTH, owner.getHealth() + RECOVER));
+        //}
+        if (owner.getInventory()[0] != 0) {
+            owner.getInventory()[0] -= 1;
+        }
     }
 
 }
