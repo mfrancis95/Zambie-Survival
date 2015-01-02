864bac25aefaa9b5dbaafdaf5a502c50a95df4c7
RTann <rosstannenbaum@Ross-Tannenbaums-iMac-2.local>
2015-01-01 20:14:29 -0500

    Merge branch 'master' into ross

--- src/main/java/org/zambiesurvival/engine/item/BarricadeItem.java
+++ src/main/java/org/zambiesurvival/engine/item/BarricadeItem.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main.java.org.zambiesurvival.engine.item;
 
 import java.awt.Graphics2D;
@@ -22,31 +21,5 @@ public class BarricadeItem extends SpawningItem {
     public void update(WorldState world) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-=======
-package main.java.org.zambiesurvival.engine.item;
 
-import java.awt.Graphics2D;
-import main.java.org.zambiesurvival.engine.entity.Barricade;
-import main.java.org.zambiesurvival.engine.state.WorldState;
-
-public class BarricadeItem extends SpawningItem {
-
-    public BarricadeItem() {
-        super(1, new Barricade());
-    }
-
-    public void render(Graphics2D g, int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void render(Graphics2D g) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-    public void update(WorldState world) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
->>>>>>> master
 }
